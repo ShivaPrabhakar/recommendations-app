dcd50d1dc43639f10705496f31b5237b3da72233
ShivaPrabhakar <47215001+ShivaPrabhakar@users.noreply.github.com>
2024-11-05 10:12:39 +0530

    Delete recommendations-app directory

--- recommendations-app/config/db.config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
-};
